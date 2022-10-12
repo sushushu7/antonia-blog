d739f5d52f15c2433562d402c48edd4dc3735ac0
Antonia <antoni.purwadiharja@gmail.com>
2022-10-12 22:58:45 +0700

    Fix SQLALCHEMY_DATABASE_URI

--- Starting+Files+-+blog-with-users-start/main.py
+++ Starting+Files+-+blog-with-users-start/main.py
@@ -18,9 +18,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://zkyxfnhjkneldf' \
-                                        ':f72059d92124746db4b0cb3600ab59826a9ad771577e95dbeb39d7368c51e605@ec2-34-249' \
-                                        '-161-200.eu-west-1.compute.amazonaws.com:5432/d649vrranvpeh '
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
